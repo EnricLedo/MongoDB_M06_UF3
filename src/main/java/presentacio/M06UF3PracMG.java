8ee9da23c9d99304c3d0b6dfb4465368f20e594e
Taufik Aoulad Ben Hmida El Halka <aoulad.ben.hmida.el.halka.taufik@alumnat.copernic.cat>
2023-03-24 09:44:29 +0000

    Taufik

--- src/main/java/presentacio/M06UF3PracMG.java
+++ src/main/java/presentacio/M06UF3PracMG.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package dam2.m06uf3pracmg;
+package presentacio;
 
 /**
  *
@@ -13,5 +13,6 @@ public class M06UF3PracMG {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello");
     }
 }
