58aa6c37cd67ed0a1e0034b4e3610abd90612c17
=Taufik Aoulad <=aoulad.ben.hmida.el.halka.taufik@alumnat.copernic.cat>
2023-03-24 10:42:35 +0100

    Creacion de las tres capas (Presentacion 'main', Logica, Entidad).

--- src/main/java/presentacio/M06UF3PracMG.java
+++ src/main/java/presentacio/M06UF3PracMG.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package dam2.m06uf3pracmg;
+package presentacio;
 
 /**
  *
