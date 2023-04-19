6c09c43ee0e0f8622188efae5d6a2a913125cb8d
Taufik Aoulad Ben Hmida El Halka <aoulad.ben.hmida.el.halka.taufik@alumnat.copernic.cat>
2023-04-19 17:15:32 +0000

    RF06 (Completada - Con alguna cosa que modificar). Se ha modificado el apartado del menu del Push. Se ha modificao la clase Entidad Fiechero y Mapeig. Se ha creado las clases Utils y Push.

---
+++ src/main/java/Utils/Utils.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utils;
+
+import java.io.File;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author Taufik
+ */
+public class Utils {
+    
+    public static String getRelativePath(File file, File baseDir) {
+        String path = file.getAbsolutePath();
+        String basePath = baseDir.getAbsolutePath();
+        if (path.startsWith(basePath)) {
+            return path.substring(basePath.length() + 1);
+        }
+        return path;
+    }
+    
+    public static List<File> listFiles(File dir) {
+        List<File> files = new ArrayList<>();
+        File[] filesArray = dir.listFiles();
+        if (filesArray != null) {
+            files.addAll(Arrays.asList(filesArray));
+            for (File file : filesArray) {
+                if (file.isDirectory()) {
+                    files.addAll(listFiles(file));
+                }
+            }
+        }
+        return files;
+    }
+    
+    public static ZoneId getZoneId() {
+        return ZoneId.systemDefault();
+    }
+}
